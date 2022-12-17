0da705743b239fd28305ba9706cd77a11d6b497c
Kirk-Koelzer <63214802+Kirk-Koelzer@users.noreply.github.com>
2022-12-17 14:39:36 -0600

    new cargo stuff

---
+++ hello_world/target/debug/deps/hello_world-055df566455729a4.d
@@ -0,0 +1,5 @@
+/home/kirk/projects/hello_world/target/debug/deps/hello_world-055df566455729a4.rmeta: src/main.rs
+
+/home/kirk/projects/hello_world/target/debug/deps/hello_world-055df566455729a4.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/deps/hello_world-ee2775f0c95c0cf4.d
@@ -0,0 +1,5 @@
+/home/kirk/projects/hello_world/target/debug/deps/hello_world-ee2775f0c95c0cf4.rmeta: src/main.rs
+
+/home/kirk/projects/hello_world/target/debug/deps/hello_world-ee2775f0c95c0cf4.d: src/main.rs
+
+src/main.rs:
