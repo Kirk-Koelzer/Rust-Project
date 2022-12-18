a81f0dfd175e498ff929cadef104b644e5a99c79
Kirk-Koelzer <63214802+Kirk-Koelzer@users.noreply.github.com>
2022-12-18 17:22:34 -0600

    average solution

--- find_the_average/src/main.rs
+++ find_the_average/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let a = 13;
     let b = 2.3;
-    let c = 120.0;
-    let d  = 3;
-    let average = (a as f64 + b + c) / d as f64; 
+    let c: f32 = 120.0;
+    let d = 3;
+    let average = (a as f64 + b + c as f64) / d as f64; 
 
     assert_eq!(average, 45.1);
     println!("Test passed!");
