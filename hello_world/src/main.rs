28b5aee533f72b4f1f46d370ec139fa1ac8e081f
Kirk-Koelzer <63214802+Kirk-Koelzer@users.noreply.github.com>
2022-12-17 20:39:45 -0600

    more bitwise operation

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
+
+//PRIMITIVE DATA TYPES
+
     let mut x:f64 = 255.05;
     x = x + 1.0;
     println!("Hello, world!");
@@ -12,6 +15,27 @@ fn main() {
     let b = 2.2;
     let c = a as f64 + b;
 
-    println!("c is equal to {}", c);
+    println!("c is equal to {:.3}", c);
+    println!("c is equal to {:08.3}", c);
+    println!("c is equal to {0:08.3}\n and a is {1} and once again c is equal to {0}", c, a);
+
+    ////bitwise ops
+    let mut value = 0b1111_0101u8;
+    println!("value is {0}.", value);
+    println!("value is {:08b}.", value);
+
+    //////Flipping the bits
+    value = !value;
+    println!("value is {:08b}.", value);
+
+    value = value & 0b1111_0111;
+    println!("value is {:08b}.", value);
+
+    println!("bit 6 is {}.", value & 0b0100_0000);
+
+
+
+
+
 
 }
