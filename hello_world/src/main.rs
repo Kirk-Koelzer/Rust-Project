c53674fdd5bf172a0cedaf46a289c686c56e2f27
Kirk-Koelzer <63214802+Kirk-Koelzer@users.noreply.github.com>
2022-12-17 19:18:48 -0600

    math and stuff

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    let mut x:u32 = 255;
-    x=x+1;
+    let mut x:f64 = 255.05;
+    x = x + 1.0;
     println!("Hello, world!");
     println!("x is {}", x);
 
-    x = 20;
+    x = 20.2;
     println!("x is {}", x);
+
+
+    let a = 10;
+    let b = 2;
+    let c = a + b;
+
+    print!("c is equal to {}", c);
+    
 }
