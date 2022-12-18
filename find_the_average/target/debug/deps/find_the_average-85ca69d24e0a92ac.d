9514b8b3a991b4bc4bfcd6c4797715b7d977ac9e
Kirk-Koelzer <63214802+Kirk-Koelzer@users.noreply.github.com>
2022-12-18 16:42:12 -0600

    test build

---
+++ find_the_average/target/debug/deps/find_the_average-85ca69d24e0a92ac.d
@@ -0,0 +1,5 @@
+/home/kirk/projects/find_the_average/target/debug/deps/find_the_average-85ca69d24e0a92ac: src/main.rs
+
+/home/kirk/projects/find_the_average/target/debug/deps/find_the_average-85ca69d24e0a92ac.d: src/main.rs
+
+src/main.rs:
---
+++ find_the_average/target/debug/find_the_average.d
@@ -0,0 +1,1 @@
+/home/kirk/projects/find_the_average/target/debug/find_the_average: /home/kirk/projects/find_the_average/src/main.rs
