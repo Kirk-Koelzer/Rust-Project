e84d88dab82d406ffe331436cf89c26bc981109b
Kirk-Koelzer <63214802+Kirk-Koelzer@users.noreply.github.com>
2022-12-18 17:01:31 -0600

    find the average

--- find_the_average/src/main.rs
+++ find_the_average/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 13;
+    let b = 2.3;
+    let c = 120.0;
+    let d  = 3;
+    let average = (a as f64 + b + c) / d as f64; 
+
+    assert_eq!(average, 45.1);
+    println!("Test passed!");
 }
