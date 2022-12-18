c42f21ff67ff75c25da83aaa4196c4f6f597fbb9
Kirk-Koelzer <63214802+Kirk-Koelzer@users.noreply.github.com>
2022-12-17 20:47:48 -0600

    comparison operators

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -32,8 +32,12 @@ fn main() {
     println!("value is {:08b}.", value);
 
     println!("bit 6 is {}.", value & 0b0100_0000);
+    
+    value = value | 0b0100_0000;
+    println!("value is {:08b}.", value);
 
-
+    value = value ^ 0b0101_0101;
+    println!("value is {:08b}.", value);
 
 
 
