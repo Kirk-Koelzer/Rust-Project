5512fd469d236a37b6d388d6cbce351ae9b777dd
Kirk-Koelzer <63214802+Kirk-Koelzer@users.noreply.github.com>
2022-12-22 14:56:12 -0600

    temp convert

--- temp/src/main.rs
+++ temp/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let celsius_temp = 23.0;
+    let fahrenheit_temp = celsius_to_fahrenheit(celsius_temp);
+
+    assert_eq!(fahrenheit_temp, 73.4);
+    println!("Test passed!");
+}
+
+fn celsius_to_fahrenheit(celsius_temp: f64) -> f64 {
+    let fahrenheit_temp: f64 = (1.8 * celsius_temp) + 32 as f64;
+    println!("The computed temp is {} degrees", fahrenheit_temp);
+    return fahrenheit_temp;
 }
