82ceca1209a51c599abf4cef6f2d31cf0484fbf9
Kirk-Koelzer <63214802+Kirk-Koelzer@users.noreply.github.com>
2022-12-25 19:15:39 -0600

    went too far down the rabbit hole

--- maxminmean/src/main.rs
+++ maxminmean/src/main.rs
@@ -1,4 +1,4 @@
-use std::array;
+use std::{array, collections::btree_map::IterMut};
 
 fn main() {
     let numbers = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
@@ -7,41 +7,38 @@ fn main() {
     let mut mean: f64 = 0.0;
     let mut meantotal: f64 = 0.0;
 
+
+    for item in numbers.iter() {
+        // println!("Current number is {}", item);
+        if item > max {
+            // println!(" current max number {} is not bigger than {}",new_max, item,);
+            max = item;
+        } else {
+            // println!(" current number {} is bigger than {}",item, new_max);
+        }
+    }
+
+    for item in numbers.iter() {
+        // println!("Current number is {}", item);
+        if item < min {
+            // println!(" current max number {} is not bigger than {}",new_min, item,);
+            min = item;
+        } else {
+            // println!(" current number {} is bigger than {}",item, new_min);
+        }
+    }
+
+    for item in numbers.iter() {
+        // println!("Current number  from array is {}", item);
+        meantotal = meantotal + item as f64;
+        // println!("Current total is {}", meantotal);
+    }
+
+    mean = meantotal / numbers.len() as f64;
+
+
     println!("{}", min);
     println!("{}", max);
     println!("{}", mean);
 }
 
-fn calc_max() {
-    for item in numbers.iter() {
-        // println!("Current number is {}", item);
-        if item > &max {
-            // println!(" current max number {} is not bigger than {}",new_max, item,);
-            max = *item;
-        } else {
-            // println!(" current number {} is bigger than {}",item, new_max);
-        }
-    }
-}
-
-fn calc_min() {
-    for item in numbers.iter() {
-        // println!("Current number is {}", item);
-        if item < &min {
-            // println!(" current max number {} is not bigger than {}",new_min, item,);
-            min = *item;
-        } else {
-            // println!(" current number {} is bigger than {}",item, new_min);
-        }
-    }
-}
-
-fn calc_mean() {
-    for item in numbers.iter() {
-        // println!("Current number  from array is {}", item);
-        meantotal = meantotal + *item as f64;
-        // println!("Current total is {}", meantotal);
-    }
-
-    mean = meantotal / numbers.len() as f64;
-}
