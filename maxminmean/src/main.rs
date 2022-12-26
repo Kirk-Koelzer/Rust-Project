344c96b176ffa4186108aad4ce4e67cee10b7ad7
Kirk-Koelzer <63214802+Kirk-Koelzer@users.noreply.github.com>
2022-12-25 18:12:24 -0600

    i have it working now let put it in functions

--- maxminmean/src/main.rs
+++ maxminmean/src/main.rs
@@ -2,53 +2,46 @@ use std::array;
 
 fn main() {
     let numbers = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
-    let mut max: i32;
-    // let mut min: i32;
-    // let mut mean: f64;
-   
-    let mut new_max: i32 = 0;  
-    let mut new_min: i32 = 0;  
+    let mut max: i32 = 0;
+    let mut min: i32 = 0;
+    let mut mean: f64 = 0.0;
+    let mut meantotal: f64 = 0.0;
 
+    println!("{}", min);
+    println!("{}", max);
+    println!("{}", mean);
+}
+
+fn calc_max() {
     for item in numbers.iter() {
         // println!("Current number is {}", item);
-        if  item > &new_max {
+        if item > &max {
             // println!(" current max number {} is not bigger than {}",new_max, item,);
-            new_max = *item;
-        }else {
+            max = *item;
+        } else {
             // println!(" current number {} is bigger than {}",item, new_max);
         }
     }
-    
+}
 
+fn calc_min() {
     for item in numbers.iter() {
         // println!("Current number is {}", item);
-        if  item < &new_min {
+        if item < &min {
             // println!(" current max number {} is not bigger than {}",new_min, item,);
-            new_min = *item;
-        }else {
+            min = *item;
+        } else {
             // println!(" current number {} is bigger than {}",item, new_min);
         }
     }
-println!("{}", new_min);
-println!("{}", new_max);
-
-
-    // calc_mean();
-    // calc_min();
-    
-
-    // assert_eq!(max, 56);
-    // assert_eq!(min, -18);
-    // assert_eq!(mean, 12.5);
-    // println!("Tests passed!");
 }
 
+fn calc_mean() {
+    for item in numbers.iter() {
+        // println!("Current number  from array is {}", item);
+        meantotal = meantotal + *item as f64;
+        // println!("Current total is {}", meantotal);
+    }
 
-
-fn calc_min(){
-    
-}
-
-fn calc_mean(){
-    
+    mean = meantotal / numbers.len() as f64;
 }
